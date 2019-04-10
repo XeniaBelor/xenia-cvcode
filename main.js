8dae5b5ef64db39ee4d22e7777989475f9ed8026
XeniaBelor <belorgames@gmail.com>
2019-04-10 23:16:37 +0300

    loading page gif

--- main.js
+++ main.js
@@ -63,3 +63,69 @@ function controlScrollToTopButton() {
     scrollToTopButton.classList.remove("show");
   }
 }
+
+var image_primarySchool = 'ps';
+var image_university = 'un';
+var image_highSchool = 'hs';
+var image_musicSchool = 'ms';
+var image_artSchool = 'as';
+
+function change(){
+  var image = document.getElementById('primarySchool');
+  if(image_primarySchool=='ps'){
+    image.src = 'images/years1.png';
+    image_primarySchool = 'pstxt'
+  } else {
+    image.src = 'images/years.png';
+    image_primarySchool = 'ps';
+  }
+}
+
+function change2(){
+  var image = document.getElementById('highSchool');
+  if(image_highSchool=='hs'){
+    image.src = 'images/years2-1.png';
+    image_highSchool = 'hstxt'
+  } else {
+    image.src = 'images/years2.png';
+    image_highSchool = 'hs';
+  }
+}
+
+function change3(){
+  var image = document.getElementById('university');
+  if(image_university=='un'){
+    image.src = 'images/years3-1.png';
+    image_university = 'untxt'
+  } else {
+    image.src = 'images/years3.png';
+    image_university = 'un';
+  }
+}
+
+function change4(){
+  var image = document.getElementById('musicSchool');
+  if(image_musicSchool=='ms'){
+    image.src = 'images/extra1.png';
+    image_musicSchool = 'mstxt'
+  } else {
+    image.src = 'images/extra.png';
+    image_musicSchool = 'ms';
+  }
+}
+
+function change5(){
+  var image = document.getElementById('artSchool');
+  if(image_artSchool=='as'){
+    image.src = 'images/extra3.png';
+    image_artSchool = 'astxt'
+  } else {
+    image.src = 'images/extra2.png';
+    image_artSchool = 'as';
+  }
+}
+
+window.addEventListener("load", function () {
+  const loader = document.querySelector(".loader");
+  loader.className += " hidden"; // class "loader hidden"
+});
