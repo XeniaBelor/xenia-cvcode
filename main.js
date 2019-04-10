8b00ef53cd1917f27c7d8c81fd24fcfd055a8825
XeniaBelor <belorgames@gmail.com>
2019-04-10 21:58:46 +0300

    change main.js, page.html, style.css
    main.js change image, page.html added educatioin section, style.css education section css

--- main.js
+++ main.js
@@ -43,4 +43,84 @@ function init(){
     new TypeWriter(txtElement, words, wait);
 }
 
+var html, body, scrollToTopButton;
+window.onload = function() {
+  html = document.documentElement;
+  body = document.body;
+  scrollToTopButton = document.getElementById("scrollToTopButton");
+};
 
+window.onscroll = controlScrollToTopButton;
+
+function controlScrollToTopButton() {
+  var windowInnerHeight = 0.5 * window.innerHeight;
+  if (
+    body.scrollTop > windowInnerHeight ||
+    html.scrollTop > windowInnerHeight
+  ) {
+    scrollToTopButton.classList.add("show");
+  } else {
+    scrollToTopButton.classList.remove("show");
+  }
+}
+
+var image_primarySchool = 'ps';
+var image_university = 'un';
+var image_highSchool = 'hs';
+var image_musicSchool = 'ms';
+var image_artSchool = 'as';
+
+function change(){
+  var image = document.getElementById('primarySchool');
+  if(image_primarySchool=='ps'){
+    image.src = 'images/years1.png';
+    image_primarySchool = 'pstxt'
+  } else {
+    image.src = 'images/years.png';
+    image_primarySchool = 'ps';
+  }
+}
+
+function change2(){
+  var image = document.getElementById('highSchool');
+  if(image_highSchool=='hs'){
+    image.src = 'images/years2-1.png';
+    image_highSchool = 'hstxt'
+  } else {
+    image.src = 'images/years2.png';
+    image_highSchool = 'hs';
+  }
+}
+
+function change3(){
+  var image = document.getElementById('university');
+  if(image_university=='un'){
+    image.src = 'images/years3-1.png';
+    image_university = 'untxt'
+  } else {
+    image.src = 'images/years3.png';
+    image_university = 'un';
+  }
+}
+
+function change4(){
+  var image = document.getElementById('musicSchool');
+  if(image_musicSchool=='ms'){
+    image.src = 'images/extra1.png';
+    image_musicSchool = 'mstxt'
+  } else {
+    image.src = 'images/extra.png';
+    image_musicSchool = 'ms';
+  }
+}
+
+function change5(){
+  var image = document.getElementById('artSchool');
+  if(image_artSchool=='as'){
+    image.src = 'images/extra3.png';
+    image_artSchool = 'astxt'
+  } else {
+    image.src = 'images/extra2.png';
+    image_artSchool = 'as';
+  }
+}
